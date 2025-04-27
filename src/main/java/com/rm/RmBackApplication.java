42bb74e7da986def391c7a1d70c808572c586e7f
jhmin <jhmin915@gmail.com>
2025-04-27 14:21:11 +0900

    initial commit

---
+++ src/main/java/com/rm/RmBackApplication.java
@@ -0,0 +1,13 @@
+package com.rm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmBackApplication.class, args);
+	}
+
+}
