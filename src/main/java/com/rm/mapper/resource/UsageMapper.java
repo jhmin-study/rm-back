897995d6fe810dc7dc88158467cba53d2c41f790
bartimaeus50 <bartimaeus50@gmail.com>
2025-05-07 19:21:01 +0900

    UsageMapper.java에@Mapper추가

--- src/main/java/com/rm/mapper/resource/UsageMapper.java
+++ src/main/java/com/rm/mapper/resource/UsageMapper.java
@@ -1,7 +1,10 @@
 package com.rm.mapper.resource;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.rm.dto.resource.Usage;
 
+@Mapper
 public interface UsageMapper {
 
 	public void createUsage(Usage usage);
