c616822f41fd88504212f19cb80b39e6d59602d4
WhiteKoreanPasta <dyddmlwlq@gmail.com>
2025-05-10 17:51:33 +0900

    User 관련 Controller,Service,Mapper클래스 추가

---
+++ src/main/java/com/rm/controller/user/UserController.java
@@ -0,0 +1,17 @@
+package com.rm.controller.user;
+
+import org.springframework.stereotype.Controller;
+
+/* ************************************************************************** */
+/* Class명 : UserController                                                   */
+/* 개발자  : 김재용                                                           */
+/* 개발일자 : 2025.05.10                                                      */
+/* 기능설명 : 회원 관련 Controller 클래스                                     */
+/* ************************************************************************** */
+@Controller
+public class UserController {
+	
+	
+	
+}
+/* ------------------------- End of UserController -------------------------- */
