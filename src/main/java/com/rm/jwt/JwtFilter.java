8aeb6e7b46f16339a3b3cd7b47764f0eacc244a0
WhiteKoreanPasta <dyddmlwlq@gmail.com>
2025-06-08 14:15:03 +0900

    user api 호출 시에만 토큰 재발급

--- src/main/java/com/rm/jwt/JwtFilter.java
+++ src/main/java/com/rm/jwt/JwtFilter.java
@@ -1,6 +1,7 @@
 package com.rm.jwt;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,7 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.rm.dto.user.CustomUserDetails;
 import com.rm.dto.user.UserDTO;
@@ -57,6 +59,42 @@ public class JwtFilter extends OncePerRequestFilter {
 		// 정상토큰
 		System.out.println("정상 토큰으로 확인됨!");
 		
+		// response를 ContentCachingResponseWraper 클래스로 변경
+		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
+		
+		filterChain.doFilter(request, responseWrapper);
+		
+		// user 관련 api호출이 아닌 경우 재발급 X
+		String reqURLStr = request.getRequestURL().toString();
+		System.out.println(reqURLStr);
+		String[] reqURL = reqURLStr.split("/");
+		for (String string : reqURL) {
+			System.out.println(string + ",");
+		}
+		if (!reqURL[3].equals("api") &&
+			!reqURL[4].equals("user")) {
+			return;
+		}
+		// responseWrapper에서 수정
+		byte[] responseArray = responseWrapper.getContentAsByteArray();
+		String responseStr = new String(responseArray, StandardCharsets.UTF_8);
+		System.out.println("response : " + response.getCharacterEncoding());
+		
+		System.out.println("responseStr : " + responseStr);
+		JsonNode node = new ObjectMapper().readTree(responseStr);
+		
+		// 빈 배열일 경우 빈 객체로 변환
+		if (responseStr.equals("[]")) {
+			responseStr = "{}";
+		}
+
+		
+		String arrayNodeName = new ArrayNode(null).getClass().getName();
+		// ArrayNode면 재발급을 진행하지 않는다.
+		if (node.getClass().getName().equals(arrayNodeName)) {
+			return;
+		}
+		
 		// 나중에 사용할 수도 있으니 Session에 토큰 payload에 넣었던 정보를 저장해 주자
 		// 단, 이때의 session은 Stateless 형태이며, 이 요청이 끝나면 사라짐
 		UserEntity user = new UserEntity();
@@ -71,10 +109,6 @@ public class JwtFilter extends OncePerRequestFilter {
 		// auth를 세션에 등록
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		
-		// response를 ContentCachingResponseWraper 클래스로 변경
-		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
-		
-		filterChain.doFilter(request, responseWrapper);
 		
 		// userId
 		String userId = customUserDetails.getUsername();
@@ -91,15 +125,7 @@ public class JwtFilter extends OncePerRequestFilter {
 //		responseBody.put("userId", userId);
 //		responseBody.put("token", token);
 		System.out.println("response 수정");
-		// responseWrapper에서 수정
-		byte[] responseArray = responseWrapper.getContentAsByteArray();
-		String responseStr = new String(responseArray, responseWrapper.getCharacterEncoding());
-		// 빈 배열일 경우 빈 객체로 변환
-		if (responseStr.equals("[]")) {
-			responseStr = "{}";
-		}
 		
-		JsonNode node = new ObjectMapper().readTree(responseStr);
 		
 		System.out.println("responseStr : " + responseStr);
 		System.out.println("node : " + node.getClass().getName());
@@ -110,12 +136,15 @@ public class JwtFilter extends OncePerRequestFilter {
 		String newResponse = new ObjectMapper().writeValueAsString(node);
 		response.setContentType("application/json");
 		response.setContentLength(newResponse.length());
-		response.getOutputStream().write(newResponse.getBytes());
+		response.getOutputStream().write(newResponse.getBytes(StandardCharsets.UTF_8));
 		
 //		response.getWriter().write(mapper.writeValueAsString(responseBody));
 //		response.addHeader("Authorization",  "Bearer " + token);
 		
 		System.out.println("토큰발급 끝 : " + newResponse);
+		System.out.println("newResponse length : " + newResponse.length());
+		System.out.println("token length : " + token.length());
+		System.out.println("response buffer size : " + response.getBufferSize());
 		
 	}
 
